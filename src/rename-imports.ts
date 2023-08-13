1409e681a1ed511730feadc78acb9784482b2e15
Artem Zakharchenko <kettanaito@gmail.com>
2023-08-13 13:06:19 +0200

    chore: remove deprecated imports, add "msw/browser"

--- src/rename-imports.ts
+++ src/rename-imports.ts
@@ -1,8 +1,31 @@
-import type { Transform } from 'jscodeshift'
+import {
+  ASTPath,
+  ImportDeclaration,
+  ImportDefaultSpecifier,
+  ImportNamespaceSpecifier,
+  ImportSpecifier,
+  Transform,
+} from 'jscodeshift'
+
+type AnyImportSpecifier =
+  | ImportSpecifier
+  | ImportDefaultSpecifier
+  | ImportNamespaceSpecifier
 
 const transformer: Transform = (file, api) => {
   const j = api.jscodeshift
   const root = j(file.source)
+
+  const rewriteImportSpecifiers = (
+    importDeclaration: ASTPath<ImportDeclaration>,
+    nextImportSpecifiers: Array<AnyImportSpecifier>,
+  ): void => {
+    importDeclaration.value.specifiers = nextImportSpecifiers
+
+    if (nextImportSpecifiers.length === 0) {
+      j(importDeclaration).remove()
+    }
+  }
 
   const mswImportDeclarations = root
     .find(j.ImportDeclaration, {
@@ -16,9 +39,13 @@ const transformer: Transform = (file, api) => {
    * Rename `rest` imports to `http`.
    */
   mswImportDeclarations.forEach((importDeclaration) => {
-    for (const importSpecifier of importDeclaration.value.specifiers!) {
+    if (!importDeclaration.value?.specifiers) {
+      return
+    }
+
+    for (const importSpecifier of importDeclaration.value.specifiers) {
       if (
-        'imported' in importSpecifier &&
+        isImportSpecifier(importSpecifier) &&
         importSpecifier.imported.name === 'rest'
       ) {
         importSpecifier.imported.name = 'http'
@@ -26,7 +53,77 @@ const transformer: Transform = (file, api) => {
     }
   })
 
+  /**
+   * Remove deprecated imports.
+   */
+  const deprecatedImports = ['response', 'context']
+
+  mswImportDeclarations.forEach((importDeclaration) => {
+    if (!importDeclaration.value?.specifiers) {
+      return
+    }
+
+    const importSpecifiers = importDeclaration.value.specifiers
+    const nextSpecifiers = importSpecifiers.filter((importSpecifier) => {
+      return (
+        isImportSpecifier(importSpecifier) &&
+        !deprecatedImports.includes(importSpecifier.imported.name)
+      )
+    })
+
+    rewriteImportSpecifiers(importDeclaration, nextSpecifiers)
+  })
+
+  /**
+   * Moves worker imports to "msw/browser".
+   */
+  const workerImports = [
+    'setupWorker',
+    'SetupWorker',
+    'SetupWorkerApi',
+    'StartOptions',
+  ]
+
+  mswImportDeclarations.forEach((importDeclaration) => {
+    if (!importDeclaration.value?.specifiers) {
+      return
+    }
+
+    const importSpecifiers = importDeclaration.value.specifiers || []
+
+    const workerImportSpecifiers: Array<ImportSpecifier> = []
+    const nextImportSpecifiers: Array<AnyImportSpecifier> = []
+
+    importSpecifiers.forEach((importSpecifier) => {
+      if (
+        isImportSpecifier(importSpecifier) &&
+        workerImports.includes(importSpecifier.imported.name)
+      ) {
+        return workerImportSpecifiers.push(importSpecifier)
+      }
+
+      nextImportSpecifiers.push(importSpecifier)
+    })
+
+    rewriteImportSpecifiers(importDeclaration, nextImportSpecifiers)
+
+    if (workerImportSpecifiers.length > 0) {
+      const mswBrowserImport = j.importDeclaration(
+        workerImportSpecifiers,
+        j.literal('msw/browser'),
+      )
+
+      j(importDeclaration).insertAfter(mswBrowserImport)
+    }
+  })
+
   return root.toSource()
+}
+
+function isImportSpecifier(
+  value: AnyImportSpecifier,
+): value is ImportSpecifier {
+  return 'imported' in value
 }
 
 export default transformer
--- test/rename-imports.test.ts
+++ test/rename-imports.test.ts
@@ -1,30 +1,78 @@
 import { applyTransform } from 'jscodeshift/src/testUtils'
 import renameImports from '../src/rename-imports'
 
-describe('rename "rest" import', () => {
-  function transform(source: string): string {
-    return applyTransform(renameImports, {}, { source })
-  }
+function transform(source: string): string {
+  return applyTransform(renameImports, {}, { source })
+}
 
-  it('renames a single "rest" import to "http"', () => {
+describe('renames the "rest" import to "http"', () => {
+  it('renames a single "rest" import', () => {
     expect(transform(`import { rest } from 'msw'`)).toBe(
       `import { http } from 'msw'`,
     )
   })
 
-  it('renames "rest" to "http" in multiple imports', () => {
-    expect(transform(`import { rest, context } from 'msw'`)).toBe(
-      `import { http, context } from 'msw'`,
+  it('renames "rest" among other imports', () => {
+    expect(transform(`import { rest, foo } from 'msw'`)).toBe(
+      `import { http, foo } from 'msw'`,
     )
 
-    expect(transform(`import { context, rest } from 'msw'`)).toBe(
-      `import { context, http } from 'msw'`,
+    expect(transform(`import { foo, rest } from 'msw'`)).toBe(
+      `import { foo, http } from 'msw'`,
     )
   })
 
   it('ignores "rest" imports from irrelevant sources', () => {
     expect(transform(`import { rest } from 'other'`)).toBe(
       `import { rest } from 'other'`,
+    )
+  })
+})
+
+describe('removes deprecated imports', () => {
+  it('removes the entire import if only deprecated imports are present', () => {
+    expect(transform(`import { response } from 'msw'`)).toBe(``)
+    expect(transform(`import { context } from 'msw'`)).toBe(``)
+    expect(transform(`import { response, context } from 'msw'`)).toBe(``)
+  })
+
+  it('removes "response" import', () => {
+    expect(transform(`import { foo, response } from 'msw'`)).toBe(
+      `import { foo } from 'msw';`,
+    )
+    expect(transform(`import { foo, response, bar } from 'msw'`)).toBe(
+      `import { foo, bar } from 'msw';`,
+    )
+  })
+
+  it('removes "context" import', () => {
+    expect(transform(`import { foo, context } from 'msw'`)).toBe(
+      `import { foo } from 'msw';`,
+    )
+    expect(transform(`import { foo, context, bar } from 'msw'`)).toBe(
+      `import { foo, bar } from 'msw';`,
+    )
+  })
+})
+
+describe('adds "msw/browser" import', () => {
+  it('moves "setupWorker" import to "msw/browser"', () => {
+    expect(transform(`import { setupWorker, rest } from 'msw'`)).toBe(
+      `\
+import { http } from 'msw';
+import { setupWorker } from "msw/browser";`,
+    )
+  })
+
+  it('moves worker types import to "msw/browser"', () => {
+    expect(
+      transform(
+        `import { SetupWorker, rest, SetupWorkerApi, StartOptions } from 'msw'`,
+      ),
+    ).toBe(
+      `\
+import { http } from 'msw';
+import { SetupWorker, SetupWorkerApi, StartOptions } from "msw/browser";`,
     )
   })
 })
